e3c9bf81c0063ff56beb1c6285f1ef5ea169d249
circlehy <janehuangyuan@gmail.com>
2019-01-26 18:53:32 +0400

    first commit

---
+++ data/my_data.py
@@ -0,0 +1,78 @@
+import os
+
+from data import common
+from data import srdata
+
+import numpy as np
+import scipy.misc as misc
+import os
+
+import torch
+import torch.utils.data as data
+
+class my_data(srdata.SRData):
+    def __init__(self, args, train=True):
+        super(my_data, self).__init__(args, train)
+        self.repeat = args.test_every // (args.n_train // args.batch_size)
+
+    def _scan(self):
+        list_hr = []
+        list_lr = [[] for _ in self.scale]
+        if self.train:
+            idx_begin = 0
+            idx_end = self.args.n_train
+        else:
+            idx_begin = self.args.n_train
+            idx_end = self.args.offset_val + self.args.n_val
+
+        all_file = os.walk(self.dir_hr)
+
+        for path_all_file,d,filelist in all_file:
+            #filename = '{:0>10}'.format(i)
+            for index_name in filelist:
+                if index_name.endswith('png'):
+                   filename = index_name[0:-4]
+                   list_hr.append(os.path.join(self.dir_hr, filename + self.ext))
+                   for si, s in enumerate(self.scale):
+                       list_lr[si].append(os.path.join(
+                       self.dir_lr,
+                       'X{}/{}x{}{}'.format(s, filename, s, self.ext)
+                       ))
+
+        print (len(list_lr))
+        return list_hr, list_lr
+
+    def _set_filesystem(self, dir_data):
+        self.apath = dir_data 
+        #self.dir_hr = os.path.join(self.apath, 'my_data/my_data_train_HR')
+        #self.dir_lr = os.path.join(self.apath, 'my_data/my_data_train_LR_bicubic')
+        self.dir_hr = '/home/hy/workspace/data/my_data/my_data_train_HR'
+        self.dir_lr = '/home/hy/workspace/data/my_data/my_data_train_LR_bicubic'
+        self.ext = '.png'
+
+    def _name_hrbin(self):
+        return os.path.join(
+            self.apath,
+            'bin',
+            '{}_bin_HR.npy'.format(self.split)
+        )
+
+    def _name_lrbin(self, scale):
+        return os.path.join(
+            self.apath,
+            'bin',
+            '{}_bin_LR_X{}.npy'.format(self.split, scale)
+        )
+
+    def __len__(self):
+        if self.train:
+            return len(self.images_hr) * self.repeat
+        else:
+            return len(self.images_hr)
+
+    def _get_index(self, idx):
+        if self.train:
+            return idx % len(self.images_hr)
+        else:
+            return idx
+
